7cd35819388b1f9a11e22f0b9e7638c25c24aa0d
steve <steve.nguyen2013@gmail.com>
2022-08-13 03:47:52 -0700

    react frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [food, setFood] = useState();
+  const [category, setCategory] = useState();
+  const [rating, setRating] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:9292/Food")
+      .then((r) => r.json())
+      .then((data) => setFood(data));
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:9292/Category")
+      .then((r) => r.json())
+      .then((data) => setCategory(data));
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:9292/Rating")
+      .then((r) => r.json())
+      .then((data) => setRating(data));
+  }, []);
+
+  console.log(food, category, rating);
+  // const displayCategory = category.map((i) => (
+  //   <div key={i.id} category={i.category}></div>
+  // ));
+
+  // const displayFood = food.map((i) => (
+  //   <div class="ui cards" key={i.id}>
+  //     <div class="header"> {displayCategory}</div>
+  //     <div class="description">
+  //       Name:{i.food_name}
+  //       Location:{i.location}
+  //     </div>
+  //     <div class="extra content">Show Like Button and Rating w/ Stars here</div>
+  //   </div>
+  // ));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div></div>
       </header>
     </div>
   );
